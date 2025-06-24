9e5dce6891faecfbe4df92bd42fc5a1517cfa8b4
sharad101 <sharadpatel801@gmail.com>
2025-06-24 11:46:12 -0400

    ShoppingCart Controller and Dao added methods

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -2,15 +2,14 @@ package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.data.UserDao;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
 
 import java.security.Principal;
@@ -54,14 +53,73 @@ public class ShoppingCartController
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
+    @PostMapping("/products/{productId}")
+    public void addProductToCart(@PathVariable int productId, Principal principal)
+    {
+        try
+        {
+            String username = principal.getName();
+            User user = userDao.getByUserName(username);
+            int userId = user.getId();
+
+            Product product = productDao.getById(productId);
+            if (product == null)
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found.");
+
+            shoppingCartDao.addProduct(userId, productId);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to add product to cart.");
+        }
+    }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
+    @PutMapping("/products/{productId}")
+    public void updateQuantity(@PathVariable int productId, @RequestBody ShoppingCartItem item, Principal principal)
+    {
+        try
+        {
+            String username = principal.getName();
+            User user = userDao.getByUserName(username);
+            int userId = user.getId();
+
+            int quantity = item.getQuantity();
+            if (quantity <= 0)
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Quantity must be greater than 0.");
+
+            shoppingCartDao.updateQuantity(userId, productId, quantity);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to update product quantity.");
+        }
+    }
 
 
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart
+    @DeleteMapping
+    public void clearCart(Principal principal)
+    {
+        try
+        {
+            String username = principal.getName();
+            User user = userDao.getByUserName(username);
+            int userId = user.getId();
+
+            shoppingCartDao.clearCart(userId);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to clear cart.");
+        }
+    }
 
 }
--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -6,4 +6,13 @@ public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+
+    // Add product to cart, increment quantity if already present
+    void addProduct(int userId, int productId);
+
+    // Update quantity of a specific product in user's cart
+    void updateQuantity(int userId, int productId, int quantity);
+
+    // Clear all products from the user's cart
+    void clearCart(int userId);
 }
